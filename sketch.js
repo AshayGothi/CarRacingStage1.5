01efc74577dbf8c542f6f49258c4e39d15835279
AshayGothi <72344439+AshayGothi@users.noreply.github.com>
2020-12-15 17:49:25 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,12 +7,10 @@ var distance = 0;
 var database;
 
 var form, player, game;
-
-var cars, car1, car2, car3, car4;
-
+var cars,car1,car2,car3,car4
 
 function setup(){
-  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  canvas = createCanvas(displayWidth=-20,displayHeight=-20);
   database = firebase.database();
   game = new Game();
   game.getState();
